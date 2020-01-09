01fcb81b00d57f30f99eb712bc56492d5a57cae8
MikaEleFant <53496914+MikaEleFant@users.noreply.github.com>
2020-01-09 08:23:55 -0800

    Debug heroku deployment

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
 'use strict';
 
 const game = new Phaser.Game(800, 768, Phaser.AUTO, 'game',false,false);
+
+game.state.add('Boot', GameCtrl.Boot);
+game.state.add('Preloader', GameCtrl.Preloader);
+game.state.add('Arena', GameCtrl.Arena);
+game.state.start('Boot');
